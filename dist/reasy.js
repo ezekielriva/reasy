549406cdcc7a5bb19b6db769a64c007d6f9d9a25
Ezekiel <ezerivadeneiral@gmail.com>
2014-07-06 17:05:32 -0300

    Change grunt directory in travis ci.

--- dist/reasy.js
+++ dist/reasy.js
@@ -100,7 +100,7 @@ var Reasy = (function () {
     //  - Your expresion already have been initializated. {expresion}
     // - - -
     startWith: function(value) {
-      if(this.exp > 0) {
+      if(this.exp.length > 0) {
         throw new Error('Your expresion already have been initializated. ' + this.exp);
       }
       this.exp += "^";
@@ -211,7 +211,7 @@ var Reasy = (function () {
     // - - -
     group: function(callback) {
       this.exp += '(';
-      callback(this).bind(this);
+      callback(this);
       this.exp += ')';
       return this;
     },
